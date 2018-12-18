c173943e212e8a8d2aae5313b59ae2ef5eefa3b8
Madhu <enometh@net.meer>
2018-12-18 19:48:41 +0530

    tabbed.c: add a comment
    if geometry is specified, tabbed window cannot be resized

--- tabbed.c
+++ tabbed.c
@@ -1027,6 +1027,9 @@ setup(void)
 	wh = 600;
 	isfixed = 0;
 
+
+	// ;madhu 181215 if geometry is specified, tabbed window
+	// ;cannot be resized
 	if (geometry) {
 		tx = ty = tw = th = 0;
 		bitm = XParseGeometry(geometry, &tx, &ty, (unsigned *)&tw,
